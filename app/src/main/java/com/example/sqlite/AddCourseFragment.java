1ed51f28dc646ad5642ba81ad90ee85d7a25f166
Elie Kheirallah <kheirallahelie@gmail.com>
2021-02-05 19:05:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/AddCourseFragment.java
@@ -0,0 +1,53 @@
+package com.example.sqlite;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class AddCourseFragment extends DialogFragment {
+    private TextView courseTitle;
+    private TextView courseCode;
+    private DatabaseHelper dbHelper;
+    private Button saveButton;
+
+    public AddCourseFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_add_course, container, false);
+        dbHelper = new DatabaseHelper(view.getContext());
+        setupUI(view);
+        return view;
+    }
+
+    private void setupUI(View view) {
+        courseCode = view.findViewById(R.id.courseCodeTextView);
+        courseTitle = view.findViewById(R.id.courseTitleTextView);
+        saveButton = view.findViewById(R.id.saveBtn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Course course = new Course(0, courseTitle.getText().toString(), courseCode.getText().toString());
+                dbHelper.insertCourse(course);
+                ((MainActivity)getActivity()).updateList();
+                getDialog().dismiss();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sqlite/Config.java
@@ -0,0 +1,14 @@
+package com.example.sqlite;
+
+public class Config {
+    // Database
+    public static final String DATABASE_NAME = "courses-db";
+
+    // Table
+    public static final String TABLE_COURSE = "course";
+
+    public static final String COLUMN_COURSE_ID = "_id";
+    public static final String COLUMN_COURSE_TITLE = "title";
+    public static final String COLUMN_COURSE_CODE = "code";
+
+}
